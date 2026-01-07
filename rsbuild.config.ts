70e3ebc9ba8104d60eed51a06e6373133a0a1c2d
liosixer-hub <360287333@qq.com>
2026-01-08 00:15:47 +0800

    fixed

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -2,12 +2,12 @@ import { defineConfig } from '@rsbuild/core';
 import { pluginReact } from '@rsbuild/plugin-react';
 import { pluginModuleFederation } from '@module-federation/rsbuild-plugin';
 import moduleFederationConfig from './module-federation.config';
-const publicUrl = process.env.PUBLIC_URL || 'http://localhost:3001/';
+const publicUrl = process.env.PUBLIC_URL || 'http://localhost';
 
 export default defineConfig({
   plugins: [pluginReact(), pluginModuleFederation(moduleFederationConfig)],
   server: {
-    port: 3001,
+    port: 80,
     htmlFallback: false,
   },
   dev: {
